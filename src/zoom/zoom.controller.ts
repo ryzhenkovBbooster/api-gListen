5cdf40c6af0a3c7df64354c271ac228a5a09226e
ryzhenkovbbooster <k.ryzhenkov@bbooster.online>
2023-11-29 17:08:41 +0300

    debug commit

--- src/zoom/zoom.controller.ts
+++ src/zoom/zoom.controller.ts
@@ -1,6 +1,6 @@
 import {Body, Controller, Get, Post, Res} from '@nestjs/common';
 import {ZoomService} from "./zoom.service";
-import { Response } from 'express';
+import {Response} from 'express';
 
 import {dirPath} from "./dto/downloadFile";
 import {createFile} from "../../api google drive/createFilee.js"
@@ -9,75 +9,107 @@ import {createFolder} from "../../api google drive/createFilee.js"
 import * as path from "path";
 
 
-
 @Controller('zoom')
 export class ZoomController {
-    constructor(private readonly zoomService: ZoomService) {}
+    constructor(private readonly zoomService: ZoomService) {
+    }
 
     @Get()
-    async hello(){
+    async hello() {
         return await this.zoomService.helloWorld()
     }
 
     @Post('validate')
-    async checkValidate(@Body() body: any, @Res() res: Response){
-        if (body.event === 'endpoint.url_validation'){
+    async checkValidate(@Body() body: any, @Res() res: Response) {
+        // Проверяем, соответствует ли событие вебхуку для проверки URL
+        if (body.event === 'endpoint.url_validation') {
+            // Валидация вебхука Zoom и сохранение лога
             const response = await this.zoomService.validateZoomWebhook(body.payload.plainToken)
             await this.zoomService.saveLog(body)
             return await res.status(200).json(response)
 
         }
-        await this.zoomService.saveLog( body)
-
-        if (body.event === "recording.completed" && dirPath.hasOwnProperty(body.payload.object.host_id) && body.payload.object.total_size > 10485760){
+        // Сохранение лога для всех событий
+        await this.zoomService.saveLog(body)
+        // Проверка, является ли событие завершением записи и соответствует ли оно условиям
+        if (body.event === "recording.completed" && dirPath.hasOwnProperty(body.payload.object.host_id) && body.payload.object.total_size > 10485760) {
             const cache = await this.zoomService.checkInCache(body.event_ts)
-            console.log("uuid: " + body.event_ts)
-            console.log('cache: ' + cache)
-
-            if (cache){
+            // Проверка на дублирование событий в кэше
+            if (cache) {
                 return res.status(200).send('dublicate')
             }
-
-            res.status(200).send('Webhook received');
+            // Формирование имени файла из времени начала и темы собрания
             let filename = `${body.payload.object.start_time} ` + `${body.payload.object.topic}`
             const account_id = body.payload.object.host_id
             filename = filename.replace(/:/g, '-').replace(/[\/\\]/g, ' ');
-            const folder_id = await  createFolder(filename, dirPath[account_id][1])
-            if(folder_id){
-                for(let file of body.payload.object.recording_files){
-                    if(file.download_url){
-                        const url = file.download_url
-                        // console.log(dirPath[account_id][1])
 
-                        const filePath= path.join(process.cwd(), '/static') + `/${filename}.mp4`;
+            //
 
-                        const local_file = await this.zoomService.downloadVideo(url, filePath)
-                        if (local_file){
+            const meetingId = body.payload.object.id
 
-                            const drive_file = await createFile(filePath, filename+'.mp4', folder_id)
-                            if (drive_file){
-                                const meetingId = body.payload.object.id
-                                const recordId = file.id
-                                const deleteRecord = await this.zoomService.deleteRecord(meetingId, recordId, filePath)
-                                if (deleteRecord){
-                                    return {err: false}
+            let currentIndex = 0;
+            const list = body.payload.object.recording_files
+            const lastIndex = list.length - 1;
+
+            // Итерация по всем файлам записи
+            for (let file of list) {
+
+                if (file.download_url) {
+                    const url = file.download_url
+                    // console.log(dirPath[account_id][1])
+
+                    const filePath = path.join(process.cwd(), '/static') + `/${filename}.mp4`;
+                    // Пропускаем файлы, которые не являются видео
+                    if (file.file_type !== "MP4") {
+                        console.log(file.file_type)
+
+                        if (currentIndex === lastIndex) {
+                            await this.zoomService.deleteRecord(meetingId, filePath)
+
+                            return res.status(200).json({data: 'Webhook received'})
+
+                        }
+                        currentIndex++;
+
+                        continue
+                    }
+                    // Загрузка видео и проверка его размера
+                    const local_file = await this.zoomService.downloadVideo(url, filePath)
+                    if (local_file === 'small') {
+                        if (currentIndex === lastIndex) {
+                            await this.zoomService.deleteRecord(meetingId, filePath)
+                            return res.status(200).json({data: 'Webhook received'})
+
+                        }
+                        currentIndex++;
+                        continue
 
 
-                                }else return false
+                    }
 
-                            }else return false
-                        }else return false
+                    // Сохранение файла в Google Drive, если он больше минимального размера
+                    if (local_file) {
+                        const folder_id = await createFolder(filename, dirPath[account_id][1])
+                        const drive_file = await createFile(filePath, filename + '.mp4', folder_id)
+                        if (drive_file) {
+                            if (currentIndex === lastIndex) {
+                                await this.zoomService.deleteRecord(meetingId, filePath)
 
-                        // await this.zoomService.downloadVideo(url, filePath)
+                                return res.status(200).json({data: 'Webhook received'})
+
+                            }
+                            currentIndex++;
 
 
-                    }else false
-                }
-            }else return false
+                        } else return res.status(404)
+                    } else return res.status(404)
 
-            // const filePath = dirPath[account_id][0] + filename
 
-        }else return await res.status(404).json({err: 'account_id undefined'})
+                } else return res.status(404)
+            }
+
+        // Возврат ошибки, если account_id не определен или другое событие
+        } else return await res.status(404).json({err: 'account_id undefined'})
 
     }
 }
--- src/zoom/zoom.service.ts
+++ src/zoom/zoom.service.ts
@@ -1,10 +1,7 @@
-import {Inject, Injectable} from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import * as crypto from 'crypto'
-import * as readline from "readline";
 import * as fs from "fs";
 import axios from "axios";
-import { promisify } from 'util';
-import * as stream from "stream";
 
 import {RedisService} from "../redis/redis.service";
 
@@ -13,9 +10,11 @@ import {RedisService} from "../redis/redis.service";
 export class ZoomService {
 
     private readonly zoomSecret: string = "InVxdBkUTOCuI3HGwHOYIA"
-    async helloWorld(){
+
+    async helloWorld() {
         return await 'hello'
     }
+
     constructor(private readonly redis: RedisService) {
     }
 
@@ -36,11 +35,11 @@ export class ZoomService {
     }
 
 
-    async saveLog(responce: any){
-        try{
+    async saveLog(responce: any) {
+        try {
             const log = JSON.stringify(responce) + '\n'
             return await fs.promises.appendFile('zoom-log.txt', log)
-        }catch (e){
+        } catch (e) {
 
             return await fs.promises.appendFile('zoom-log.txt', responce + '\n')
 
@@ -48,103 +47,118 @@ export class ZoomService {
     }
 
 
-    async downloadVideo(url: string, filePath: string ){
-        try{
+    async downloadVideo(url: string, filePath: string) {
+        try {
+
+
             const response = await axios.get(url, {
                 responseType: 'stream'
             })
-            const finished = promisify(stream.finished);
-            const writer =  fs.createWriteStream(filePath)
+
+
+            // const finished = promisify(stream.finished);
+            const writer = fs.createWriteStream(filePath)
+
             response.data.pipe(writer)
-            await finished(writer)
+            await new Promise((resolve, reject) => {
+                writer.on('finish', resolve);
+                writer.on('error', reject);
+            });
+            const stats = fs.statSync(filePath);
+            if (stats.size < 10 * 1024 * 1024) {
+                // Если размер файла меньше 10 МБ, удаляем его
+                fs.unlinkSync(filePath);
+                return 'small';
+            }
+
+
             return true
 
-        }
-        catch (error){
+        } catch (error) {
             await this.saveLog("false_load_file_to_localMachine" + error)
 
             return false
         }
 
     }
-    async deleteRecord(meetingId: string, recordId: string, filePath: string){
+
+    async deleteRecord(meetingId: string, filePath: string) {
         const accessToken = 'eyJzdiI6IjAwMDAwMSIsImFsZyI6IkhTNTEyIiwidiI6IjIuMCIsImtpZCI6IjNkOTczNDJjLWY0MzMtNDFkYi05ZDhmLTQ5ODEwNDdlNmE3ZSJ9.eyJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJUWHdkWkNtRFF0Mjh4SHlLOGJ2WmJ3IiwidmVyIjo5LCJhdWlkIjoiNjc4MWUzYzAxMzBkOTI0MTliNzVjNTExMzYzYjdkMDAiLCJuYmYiOjE2OTk5NzU0MjgsImNvZGUiOiJmN3Q0T3dLQVJPT3dGeTJjLTdTQnJnNTFoNkNIVlpmeU8iLCJpc3MiOiJ6bTpjaWQ6VFh0Wk9KQ1dSR04xSkJuQk41RzRnIiwiZ25vIjowLCJleHAiOjE2OTk5NzkwMjgsInR5cGUiOjMsImlhdCI6MTY5OTk3NTQyOCwiYWlkIjoiMzZpa3dDUHlRN0t1bDJMbmdDMXpHUSJ9.wupmgsz6Y47Jitk0vTwJNs-Nfo4x7OhUJGKczehCPQRZjvylxAlzy2Wp4aXR3_KyJY0B7glwAvKXrRPHP_W_GQ'
-        const url = `https://api.zoom.us/v2/meetings/${meetingId}/recordings/${recordId}?action=trash`
+        const url = `https://api.zoom.us/v2/meetings/${meetingId}/recordings?action=trash`
         try {
             await axios.delete(url, {
-                headers:{
+                headers: {
                     'Authorization': `Bearer ${accessToken}`
                 }
             })
 
 
-
-        }catch (err){
-            if (err.response && err.response.status === 401 ){
+        } catch (err) {
+            if (err.response && err.response.status === 401) {
                 let newToken = await this.updateToken()
                 newToken = newToken.replace(/\s+/g, '');
                 await axios.delete(url, {
-                    headers:{
+                    headers: {
                         'Authorization': `Bearer ${newToken}`
                     }
                 })
 
-            }else{
+            } else {
                 await this.saveLog("false_delete_record_on_zoom: " + err)
                 return false
             }
-
 
 
         }
         try {
             await fs.promises.unlink(filePath)
             return true
-        }catch (err){
+        } catch (err) {
 
             await this.saveLog("false_delete_record_on_localmachine" + err)
             return false
 
         }
     }
-    private async  updateToken() {
-    var url = "https://zoom.us/oauth/token";
-    var CLIENT_ID = "TXtZOJCWRGN1JBnBN5G4g";
-    var CLIENT_SECRET = "6dgymG1QMu2VerJZHgskAndTEcznNGin";
 
-    const base64Credentials = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
-    const authorizationHeader = `Basic ${base64Credentials}`;
+    private async updateToken() {
+        var url = "https://zoom.us/oauth/token";
+        var CLIENT_ID = "TXtZOJCWRGN1JBnBN5G4g";
+        var CLIENT_SECRET = "6dgymG1QMu2VerJZHgskAndTEcznNGin";
 
-    try {
-        const params = {
-            grant_type: "account_credentials",
-            account_id: "36ikwCPyQ7Kul2LngC1zGQ"
-        }
-        const res = await axios.post(url, params, {
-            headers: {
-                "Authorization": authorizationHeader,
-                "Content-Type": "application/x-www-form-urlencoded"
+        const base64Credentials = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
+        const authorizationHeader = `Basic ${base64Credentials}`;
+
+        try {
+            const params = {
+                grant_type: "account_credentials",
+                account_id: "36ikwCPyQ7Kul2LngC1zGQ"
             }
-        });
+            const res = await axios.post(url, params, {
+                headers: {
+                    "Authorization": authorizationHeader,
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            });
 
-        if (res) {
-            return res.data.access_token;
+            if (res) {
+                return res.data.access_token;
+            }
+        } catch (err) {
+            // Обработка ошибок
+            throw err;
         }
-    } catch (err) {
-        // Обработка ошибок
-        throw err;
-    }
     }
 
-    async checkInCache(uuid){
+    async checkInCache(uuid) {
         const cacheData = await this.redis.get(uuid)
         console.log(cacheData)
 
-        if (cacheData){
+        if (cacheData) {
             return true
-        }else{
+        } else {
 
-            await this.redis.set(uuid, 'cashed', 7200 )
+            await this.redis.set(uuid, 'cashed', 7200)
             return false
         }
     }
